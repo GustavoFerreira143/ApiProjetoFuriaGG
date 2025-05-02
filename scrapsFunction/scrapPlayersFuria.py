049a18756c8dadd8fdf9acd3d4ac776850280a76
Gustavo Ferreira de Oliveira Coelho <gustavo.feneita1@gmail.com>
2025-05-02 19:04:36 -0300

    Correcao Scrap

--- scrapsFunction/scrapPlayersFuria.py
+++ scrapsFunction/scrapPlayersFuria.py
@@ -5,52 +5,42 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-
 def obter_dados_plyers(url):
-
-
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
 
-    # Inicializar o navegador
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    jogadores_furia = []  
+    jogadores_furia = []
 
     try:
-        # Acessar o site
         driver.get(url)
-
-        # Esperar carregar o site
-        time.sleep(5) 
-
-        # Buscar os elementos com o nome dos jogadores
+        time.sleep(5)
         player_elements = driver.find_elements(By.CLASS_NAME, 'font-bold')
-
-        # Extrair os nomes
         player_names = [element.text for element in player_elements if element.text.strip() != ""]
 
-        # Variável de controle
         start_collecting = False
 
-        # Iterar sobre os elementos
         for element in player_names:
-            text = element.strip().upper()  # Manipula a string para uppercase
+            text = element.strip().upper()
             if not text:
                 continue
 
-            if "ELENCO" in text:  # Pesquisa se a string contém 'ELENCO'
+            if "ELENCO" in text:
                 start_collecting = True
                 continue
 
             if start_collecting:
-                if "FURIA" in text:  # Pesquisa por 'FURIA'
+                if "FURIA" in text:
                     break
                 jogadores_furia.append(text)
     except Exception as e:
-        return (f"Ocorreu um erro")
+        return f"Ocorreu um erro: {str(e)}"
     finally:
         driver.quit()
+
     return jogadores_furia
